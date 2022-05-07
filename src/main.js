47594ca58003f8f37ab0ad335abf70389517b2e7
Albert Bairamukov <90350959+Albertoejdhdjfjdjfj@users.noreply.github.com>
2022-05-07 21:58:47 +0300

    I correct lesson-2

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-
+let user = 'John Doe';
+console.log(user);
+const student = 'Albert Bairamukov';
+console.log(student);
+user += student; // происходит контентенация строк переменных
+console.log(user);
+let test = 1;
+test++;
+test += '1'; //происходит контетенация переменных как строки
+console.log(test);
+test--; // просто отнимется 1 как от числа
+console.log(test);
