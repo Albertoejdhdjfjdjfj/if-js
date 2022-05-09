f1557cf8b3395cb7922c8b598f570bb8061cec0c
Albert Bairamukov <90350959+Albertoejdhdjfjdjfj@users.noreply.github.com>
2022-05-09 19:11:22 +0300

    I correct lesson-2

--- src/main.js
+++ src/main.js
@@ -21,15 +21,15 @@ console.log(test);
 let arr = [2, 3, 5, 8];
 let sum = 0;
 for (let i = 0; i < arr.length; i++) {
-    sum += arr[i]
+  sum += arr[i];
 }
 console.log(sum);
 
 arr = [2, 5, 8, 15, 0, 6, 20, 3];
 for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 5 && arr[i] < 10) console.log(arr[i]);
+  if (arr[i] > 5 && arr[i] < 10) console.log(arr[i]);
 }
 
 for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 == 0) console.log(arr[i]);
+  if (arr[i] % 2 == 0) console.log(arr[i]);
 }
