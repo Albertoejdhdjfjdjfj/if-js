f6cc51f4dd81a9ccf02cbb2552c535667b868944
Albert Bairamukov <90350959+Albertoejdhdjfjdjfj@users.noreply.github.com>
2022-06-03 18:21:50 +0300

    I correct lesson-4

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
+
+const colorchange = () => {
+  let i = 0;
+  return function (event) {
+    event.target.style.color = colors[i];
+    i + 1 == colors.lenght ? (i = 0) : i++;
+  };
+};
+
 function sum(a) {
   let Sum = a;
   function f(b) {
@@ -9,19 +18,10 @@ function sum(a) {
   };
   return f;
 }
-
 console.log(sum(5)(2));
 
 //====================================
 const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
-const colorchange = () => {
-  let i = 0;
-  return function (event) {
-    event.target.style.color = colors[i];
-    i + 1 == colors.lenght ? (i = 0) : i++;
-  };
-};
-
 const colorchange1 = colorchange();
 const colorchange2 = colorchange();
 const colorchange3 = colorchange();
@@ -33,4 +33,4 @@ p2.addEventListener('click', colorchange2);
 const p3 = document.getElementById('text3');
 p3.addEventListener('click', colorchange3);
 
-// export{sum};
+// module.exports = { sum,colors,colorchange };
---
+++ testFunctions.test.js
@@ -0,0 +1,28 @@
+// const { sum } = require('./main');
+
+   function sum(a) {
+     let Sum = a;
+     function f(b) {
+       Sum += b;
+       return Sum;
+     }
+     f.toString = function () {
+       return Sum;
+     };
+     return f;
+   }
+
+
+describe('Check function Sum', () => {
+  test('Check with value 2&&-2', () => {
+    expect(sum(2)(-2)).toBe(0);
+  });
+  test('Check with value 2 && 3', () => {
+    expect(sum(2)(3)).toBe(5);
+  });
+  test('Check with value -2 && 100', () => {
+    expect(sum(-2)(100)).toBe(98);
+  });
+});
+
+
