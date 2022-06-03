2c6e94457294a26aaa49dedc8b9567de269a41f9
Albert Bairamukov <90350959+Albertoejdhdjfjdjfj@users.noreply.github.com>
2022-06-03 03:13:15 +0300

    The test not going

--- src/main.js
+++ src/main.js
@@ -33,4 +33,6 @@ p2.addEventListener('click', colorchange2);
 const p3 = document.getElementById('text3');
 p3.addEventListener('click', colorchange3);
 
-// module.exports = sum();
+
+
+module.exports = sum();
