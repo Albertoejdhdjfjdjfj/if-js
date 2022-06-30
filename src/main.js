0b7b3323434ecd61bf0aedde9f2102dba2e99f5e
Albert Bairamukov <90350959+Albertoejdhdjfjdjfj@users.noreply.github.com>
2022-06-30 17:16:28 +0300

    I complete lesson4

--- src/main.js
+++ src/main.js
@@ -1,35 +1,32 @@
-
-const colorchange = () => {
-  let i = 0;
+const changecolor = () => {
+  var i = 0;
   return function (event) {
     event.target.style.color = colors[i];
-    i + 1 == colors.lenght ? (i = 0) : i++;
+    if(i>colors.length-2) return i=0;
+    else return i++;
   };
 };
-
 function sum(a) {
-  let Sum = a;
+  let sum = a;
   function f(b) {
-    Sum += b;
-    return Sum;
+    sum += b;
+    return sum;
   }
   f.toString = function () {
-    return Sum;
+    return sum;
   };
   return f;
 }
 console.log(sum(5)(2));
-
 //====================================
 const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
-const colorchange1 = colorchange();
-const colorchange2 = colorchange();
-const colorchange3 = colorchange();
-
+const changecolor1 = changecolor();
+const changecolor2 = changecolor();
+const changecolor3 = changecolor();
 const p1 = document.getElementById('text1');
-p1.addEventListener('click', colorchange1);
 const p2 = document.getElementById('text2');
-p2.addEventListener('click', colorchange2);
 const p3 = document.getElementById('text3');
-p3.addEventListener('click', colorchange3);
+p1.addEventListener('click', changecolor1);
+p2.addEventListener('click', changecolor2);
+p3.addEventListener('click', changecolor3);
 
--- testFunctions.test.js
+++ testFunctions.test.js
@@ -1,15 +1,31 @@
-
+/**
+ * @jest-environment jsdom
+ */
+ document.body.innerHTML = `  
+ <p id="text1">Text 1</p>
+ <p id="text2">Text 2</p>
+ <p id="text3">Text 3</p>`;
    function sum(a) {
-     let Sum = a;
+     let sum = a;
      function f(b) {
-       Sum += b;
-       return Sum;
+       sum += b;
+       return sum;
      }
      f.toString = function () {
-       return Sum;
+       return sum;
      };
      return f;
    }
+
+   const changecolor = () => {
+    var i = 0;
+    return function (event) {
+      const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+      event.target.style.color = colors[i];
+      if(i>colors.length-2) return i=0;
+      else return i++;
+    };
+  };
 
 
 describe('Check function Sum', () => {
@@ -24,4 +40,69 @@ describe('Check function Sum', () => {
   });
 });
 
+describe("check out change text color", () => {
+const changecolor1 = changecolor();
+const changecolor2 = changecolor();
+const changecolor3 = changecolor();
+const p1 = document.getElementById('text1');
+const p2 = document.getElementById('text2');
+const p3 = document.getElementById('text3');
+p1.addEventListener('click', changecolor1);
+p2.addEventListener('click', changecolor2);
+p3.addEventListener('click', changecolor3);
+  let pBlock = [p1, p2, p3];
+  const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+  test("expect 1st click color is magenta", async () => {
+    for (let i = 0; i < pBlock.length; i++) {
+      pBlock[i].click();
+      changecolor(pBlock[i]);
+      const color = pBlock[i].style.color;
+      expect(color).toBe("magenta");
+    }
+  });
 
+  test("expect 2st click color is cyan", async () => {
+    for (let i = 0; i < pBlock.length; i++) {
+      pBlock[i].click();
+      changecolor(pBlock[i]);
+      const color = pBlock[i].style.color;
+      expect(color).toBe("cyan");
+    }
+  });
+
+  test("expect 3st click color is firebrick", async () => {
+    for (let i = 0; i < pBlock.length; i++) {
+      pBlock[i].click();
+      changecolor(pBlock[i]);
+      const color = pBlock[i].style.color;
+      expect(color).toBe("firebrick");
+    }
+  });
+
+  test("expect 4th click color is springgreen", async () => {
+    for (let i = 0; i < pBlock.length; i++) {
+      pBlock[i].click();
+      changecolor(pBlock[i]);
+      const color = pBlock[i].style.color;
+      expect(color).toBe("springgreen");
+    }
+  });
+
+  test("expect 5th click color is skyblue", async () => {
+    for (let i = 0; i < pBlock.length; i++) {
+      pBlock[i].click();
+      changecolor(pBlock[i]);
+      const color = pBlock[i].style.color;
+      expect(color).toBe("skyblue");
+    }
+  });
+
+  test("check cycle actions", async () => {
+    for (let i = 0; i < pBlock.length; i++) {
+      pBlock[i].click();
+      changecolor(pBlock[i]);
+      const color = pBlock[i].style.color;
+      expect(color).toBe("magenta");
+    }
+  });
+});
