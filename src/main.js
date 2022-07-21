57eda99a88c3f4d89bb30c47488b3f0ac53ec6f6
Albert Bairamukov <90350959+Albertoejdhdjfjdjfj@users.noreply.github.com>
2022-07-21 12:20:31 +0300

    I corrected lesson-7

--- src/main.js
+++ src/main.js
@@ -42,7 +42,7 @@ function deepEqual(obj1, obj2) {
       // Проверка на одинаковое количество свойств
       return false;
     }
-    for (let propName in obj1) {
+    for (const propName in obj1) {
       if (!obj2.hasOwnProperty(propName)) {
         // Есть ли свойства в обоих объектах
         return false;
