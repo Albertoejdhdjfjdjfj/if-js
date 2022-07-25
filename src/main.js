17c233fb171b229377727e69ea15c7eb4825b7e6
Albert Bairamukov <90350959+Albertoejdhdjfjdjfj@users.noreply.github.com>
2022-07-25 12:52:56 +0300

    I completed lesson-8

--- src/main.js
+++ src/main.js
@@ -1,70 +1,79 @@
-class User{
-  constructor(firstName,lastName){
-      this.firstName=firstName;
-      this.lastName=lastName;
+class User {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
   }
-  fullName(){
-    return this.firstName+' '+this.lastName;
+  fullName() {
+    return this.firstName + " " + this.lastName;
   }
 }
 
-class Student extends User{
-  constructor(firstName,lastName,admissionYear,courseName){
-   super(firstName,lastName);
-   this.admissionYear=admissionYear;
-   this.courseName=courseName;
+class Student extends User {
+  constructor(firstName, lastName, admissionYear, courseName) {
+    super(firstName, lastName);
+    this.admissionYear = admissionYear;
+    this.courseName = courseName;
   }
-  course(){
-    
-    return super.fullName()+'-'+ this.admissionYear+','+this.courseName;
+  course() {
+    return super.fullName() + "-" + this.admissionYear + "," + this.courseName;
   }
-
 }
 
-class Students{
-  constructor(studentsData){
-  this.studentsData=studentsData;    
+class Students {
+  constructor(studentsData) {
+    this.studentsData = studentsData;
+  }
+  getInfo() {
+    const sortdata = this.studentsData;
+    const res = [];
+    sortdata.sort((a, b) => (a.admissionYear > b.admissionYear ? -1 : 1));
+    sortdata.forEach((element) => {
+      res.push(
+        element.firstName +
+          " " +
+          element.lastName +
+          "-" +
+          element.courseName +
+          "," +
+          (2019 - element.admissionYear + 1) +
+          " курс",
+      );
+    });
+    return res;
+  }
 }
-getInfo(){
-  let sortdata=this.studentsData;
-  let res=[]
-  sortdata.sort((a, b) => a.admissionYear > b.admissionYear ? -1 : 1);
-  sortdata.forEach(element => {
-    res.push(element.firstName+' '+element.lastName+'-'+element.courseName+','+(2019-element.admissionYear+1)+' курс');
-  });
-  return res
-}
-}
-let user=new User('Григорий','Лепс');
-let student=new Student('Григорий','Лепс',1982,4)
-console.log(user.fullName())
-console.log(student.course())
+
+const user = new User("Григорий", "Лепс");
+const student = new Student("Григорий", "Лепс", 1982, 4);
+console.log(user.fullName());
+console.log(student.course());
+
 const studentsData = [
   {
-    firstName: 'Василий',
-    lastName: 'Петров',
+    firstName: "Василий",
+    lastName: "Петров",
     admissionYear: 2019,
-    courseName: 'Java',
+    courseName: "Java",
   },
   {
-    firstName: 'Иван',
-    lastName: 'Иванов',
+    firstName: "Иван",
+    lastName: "Иванов",
     admissionYear: 2018,
-    courseName: 'JavaScript',
+    courseName: "JavaScript",
   },
   {
-    firstName: 'Александр',
-    lastName: 'Федоров',
+    firstName: "Александр",
+    lastName: "Федоров",
     admissionYear: 2017,
-    courseName: 'Python',
+    courseName: "Python",
   },
   {
-    firstName: 'Николай',
-    lastName: 'Петров',
+    firstName: "Николай",
+    lastName: "Петров",
     admissionYear: 2019,
-    courseName: 'Android',
-  }
+    courseName: "Android",
+  },
 ];
 
-const students=new Students(studentsData);
-console.log(students.getInfo())
+const students = new Students(studentsData);
+console.log(students.getInfo());
