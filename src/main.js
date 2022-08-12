e9ec7bfe8b9a2ae83cad35e570e076823d1ca045
Albert Bairamukov <90350959+Albertoejdhdjfjdjfj@users.noreply.github.com>
2022-08-12 13:28:23 +0300

    I corrected lesson 9

--- src/main.js
+++ src/main.js
@@ -1,40 +1,34 @@
-// const changecolor = () => {
-//   let i = 0;
-//   return function (event) {
-//     event.target.style.color = colors[i];
-//     if (i > colors.length - 2) return (i = 0);
-//     return i++;
-//   };
-// };
-
-const changecolor = () => (event) => {
-  event.target.style.color = colors.next();
-};
-
-//====================================
 const colors = {
   data: ["magenta", "cyan", "firebrick", "springgreen", "skyblue"],
   [Symbol.iterator]() {
-    this.current = this.data;
-    this.i = 0;
-    return {
-      next() {
-        if (this.i > this.current.length - 2) {
-          this.i = 0;
-          return { done: false, value: this.current[this.i] };
-        } else {
-          return { done: false, value: this.current[this.i++] };
-        }
-      },
+    let i = 0;
+    const next = () => {
+      if (i < this.data.length) {
+        return { value: this.data[(i += 1)], done: false };
+      }
+
+      if (i === this.data.length) {
+        i = 0;
+        return { value: this.data[(i += 1)], done: false };
+      }
+
+      return i++;
     };
+
+    return { next };
   },
 };
-const changecolor1 = changecolor();
-const changecolor2 = changecolor();
-const changecolor3 = changecolor();
+
+const changecolor = (p, colors) => {
+  const col = colors[Symbol.iterator]();
+  p.onclick = () => {
+    p.style.color = col.next().value;
+  };
+};
+
 const p1 = document.getElementById("text1");
 const p2 = document.getElementById("text2");
 const p3 = document.getElementById("text3");
-p1.addEventListener("click", changecolor1);
-p2.addEventListener("click", changecolor2);
-p3.addEventListener("click", changecolor3);
+changecolor(p1, colors);
+changecolor(p2, colors);
+changecolor(p3, colors);
