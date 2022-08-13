b0172de509ffd3175584ea2559c1c3e36d4a99d9
Albert Bairamukov <90350959+Albertoejdhdjfjdjfj@users.noreply.github.com>
2022-08-13 13:55:49 +0300

    I corrected lesson-10

--- src/main.js
+++ src/main.js
@@ -69,30 +69,27 @@ const slider = document.getElementById("wrapper_for_category");
 const button = document.createElement("div");
 button.classList.add("button_arrow_right");
 button.innerHTML = "<svg><use xlink:href=\"#arrow_right\"></use></svg> ";
-sliderImages(slider, data, button, 0, 4);
-changeImages(button, slider, data, 4);
+sliderImages(slider, data, button, 4);
 button.addEventListener("click", changeImages(slider, data, button, 4));
 
-function sliderImages(blockbody, data, button, i, lenght) {
-  for (let start = i; start < lenght + i; start++) {
+function sliderImages(blockbody, data, button, lenght) {
+  for (let start = 0; start < lenght; start++) {
     const object = document.createElement("div");
     object.classList.add("object");
     object.innerHTML = `<a href=""><img src="${data[start].imageUrl}"></a><h3>${data[start].name}</h3> <p>${data[start].city}, ${data[start].country}</p>`;
     blockbody.appendChild(object);
-    if (start == data.lenght - lenght) {
-      break;
-    }
   }
 
   blockbody.appendChild(button);
 }
 
 function changeImages(blockbody, data, button, lenght) {
-  let start = lenght - 1;
-  return function () {
-    blockbody.innerHTML = "";
-    sliderImages(blockbody, data, button, start, lenght);
-    if (start == data.length - lenght) return (start = 0);
-    return start++;
-  };
+  let copydata=data
+  return function (){
+    blockbody.innerHTML='';
+    copydata.shift();
+    sliderImages(blockbody, data, button, lenght)
+    if(copydata.lenght==lenght+1) copydata+=(data);
+    return copydata;
+  }
 }
