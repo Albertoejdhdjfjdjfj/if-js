f2e15ae4c1068e968796d1375d66204e52bed9e7
Albert Bairamukov <90350959+Albertoejdhdjfjdjfj@users.noreply.github.com>
2022-08-19 16:38:15 +0300

    I created new files for lesson 11

--- src/js/main.js
+++ src/js/main.js
@@ -93,41 +93,41 @@ function changeImages(blockbody, data, button, size) {
     console.log(copydata);
   };
 }
-let windo=document.getElementById("filter_form_body");
-windo.style.display='none';
-let input=document.getElementById("num_people_input");
+const windo=document.getElementById("filter_form_body");
+windo.style.display="none";
+const input=document.getElementById("num_people_input");
 
-input.addEventListener('mouseover',()=>{
-  windo.style.display='flex';
+input.addEventListener("mouseover",()=>{
+  windo.style.display="flex";
 })
 
-addEventListener('dblclick',()=>{
-  windo.style.display='none';
+addEventListener("dblclick",()=>{
+  windo.style.display="none";
 })
 
-let popUpLabel=document.getElementById('popUplabel_children');
-let popUpSelect=document.getElementById('popUpselect_children');
-popUpLabel.style.display='none';
-popUpSelect.style.display='none';
+const popUpLabel=document.getElementById("popUplabel_children");
+const popUpSelect=document.getElementById("popUpselect_children");
+popUpLabel.style.display="none";
+popUpSelect.style.display="none";
 
-let buttonPlus_adults=document.getElementById('plus_adults');
-let buttonMinus_adults=document.getElementById('minus_adults');
-let inputField_adults=document.getElementById('input_field_adults');
+const buttonPlus_adults=document.getElementById("plus_adults");
+const buttonMinus_adults=document.getElementById("minus_adults");
+const inputField_adults=document.getElementById("input_field_adults");
 changeNumber(buttonPlus_adults,buttonMinus_adults,inputField_adults,30);
 
-let buttonPlus_children=document.getElementById('plus_children');
-let buttonMinus_children=document.getElementById('minus_children');
-let inputField_children=document.getElementById('input_field_children');
+const buttonPlus_children=document.getElementById("plus_children");
+const buttonMinus_children=document.getElementById("minus_children");
+const inputField_children=document.getElementById("input_field_children");
 changeNumber(buttonPlus_children,buttonMinus_children,inputField_children,10,popUpLabel,popUpSelect);
 
-let buttonPlus_rooms=document.getElementById('plus_rooms');
-let buttonMinus_rooms=document.getElementById('minus_rooms');
-let inputField_rooms=document.getElementById('input_field_rooms');
+const buttonPlus_rooms=document.getElementById("plus_rooms");
+const buttonMinus_rooms=document.getElementById("minus_rooms");
+const inputField_rooms=document.getElementById("input_field_rooms");
 changeNumber(buttonPlus_rooms,buttonMinus_rooms,inputField_rooms,30);
 
 function changeNumber(buttonPlus,buttonMinus,inputField,max,popUpLabel=undefined,popUpSelect=undefined){
   let i = inputField.textContent;
-  buttonPlus.addEventListener('click',()=> {
+  buttonPlus.addEventListener("click",()=> {
     if(i<max){i++;  inputField.textContent=i;}
     else {i = 0;inputField.textContent=i;}
     if(popUpLabel&&popUpSelect){
@@ -135,9 +135,9 @@ function changeNumber(buttonPlus,buttonMinus,inputField,max,popUpLabel=undefined
     }
   })
 
-  buttonMinus.addEventListener('click',()=> {
+  buttonMinus.addEventListener("click",()=> {
     if(i>0){i--;  inputField.textContent=i;}
-    else {i = max; inputField.textContent=i;};
+    else {i = max; inputField.textContent=i;}
     if(popUpLabel&&popUpSelect){
       popUpLabelAndSelect(inputField_children,popUpLabel,popUpSelect);
     }
@@ -146,11 +146,11 @@ function changeNumber(buttonPlus,buttonMinus,inputField,max,popUpLabel=undefined
 
  function popUpLabelAndSelect(inputField_children,popUpLabel,popUpSelect){
    if(inputField_children.textContent!=0){
-    popUpLabel.style.display='inline';
-    popUpSelect.style.display='block';
+    popUpLabel.style.display="inline";
+    popUpSelect.style.display="block";
    }
   else if(inputField_children.textContent==0){
-    popUpLabel.style.display='none';
-    popUpSelect.style.display='none'; 
+    popUpLabel.style.display="none";
+    popUpSelect.style.display="none"; 
   }
 }
