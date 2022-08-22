bb812a033cf013a5bd4d69c096ff25e7d1fca0b3
Albert Bairamukov <90350959+Albertoejdhdjfjdjfj@users.noreply.github.com>
2022-08-22 20:04:51 +0300

    I corrected lesson-11

--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,6 @@ const input = document.getElementById("num_people_input");
 input.addEventListener("click", clickInput());
 
 const popUpLabel = document.getElementById("popUplabel_children");
-// let popUpSelect ;
-// popUpSelect.innerHTML="<select><option>0 years old</option><option>1 years old</option><option>2 years old</option><option>3 years old</option><option>4 years old</option><option>5 years old</option><option>6 years old</option><option>7 years old</option><option>8 years old</option></select>";
 popUpLabel.style.display = "none";
 
 const buttonPlus_adults = document.getElementById("plus_adults");
@@ -17,27 +15,14 @@ changeNumber(buttonPlus_adults, buttonMinus_adults, inputField_adults, 30);
 const buttonPlus_children = document.getElementById("plus_children");
 const buttonMinus_children = document.getElementById("minus_children");
 const inputField_children = document.getElementById("input_field_children");
-changeNumber(
-  buttonPlus_children,
-  buttonMinus_children,
-  inputField_children,
-  10,
-  popUpLabel
-);
+changeNumber(buttonPlus_children, buttonMinus_children, inputField_children, 10, popUpLabel);
 
 const buttonPlus_rooms = document.getElementById("plus_rooms");
 const buttonMinus_rooms = document.getElementById("minus_rooms");
 const inputField_rooms = document.getElementById("input_field_rooms");
 changeNumber(buttonPlus_rooms, buttonMinus_rooms, inputField_rooms, 30);
 
-function changeNumber(
-  buttonPlus,
-  buttonMinus,
-  inputField,
-  max,
-  popUpLabel = undefined,
-  popUpSelect = undefined,
-) {
+function changeNumber(buttonPlus, buttonMinus, inputField, max, popUpLabel = undefined) {
   let i = inputField.textContent;
 
   buttonPlus.addEventListener("click", () => {
@@ -49,8 +34,8 @@ function changeNumber(
       inputField.textContent = i;
     }
 
-    if (popUpLabel && popUpSelect) {
-      popUpLabelAndSelect(inputField_children, popUpLabel, popUpSelect);
+    if (popUpLabel) {
+      popUpLabelAndSelect(inputField_children, popUpLabel);
     }
 
     inputParametrs();
@@ -65,10 +50,10 @@ function changeNumber(
       i = max;
       inputField.textContent = i;
     }
-    if (popUpLabel && popUpSelect) {
-      popUpLabelAndSelect(inputField_children, popUpLabel, popUpSelect);
+    if (popUpLabel) {
+      popUpLabelAndSelect(inputField_children, popUpLabel);
     }
-    inputParametrs() 
+    inputParametrs();
     selectNum();
   });
 }
@@ -98,11 +83,20 @@ function inputParametrs() {
   input.textContent = `${inputField_adults.textContent} Adults — ${inputField_children.textContent} Children — ${inputField_rooms.textContent} Room`;
 }
 
-function selectNum(){
-  //  const selects=document.getElementsByClassName("popUpselect_children")
-  //  if(selects.length!=Number(inputField_children.textContent)){
-  //   for(let i=selects.length;i<=Number(inputField_children.textContent);i++){
-      
-  //   }
-  //  }
+function selectNum() {
+  const selects = document.getElementsByClassName("popUpselect_children");
+  if (selects.length < inputField_children.textContent) {
+    for (let i = selects.length; i < inputField_children.textContent; i++) {
+      const popUpSelect = document.createElement("select");
+      popUpSelect.classList = "popUpselect_children";
+      popUpSelect.id = "popUpselect_children";
+      popUpSelect.innerHTML =
+        "<option>0 years old</option><option>1 years old</option><option>2 years old</option><option>3 years old</option><option>4 years old</option><option>5 years old</option><option>6 years old</option><option>7 years old</option><option>8 years old</option>";
+      windo.appendChild(popUpSelect);
+    }
+  } else if (selects.length > inputField_children.textContent) {
+    for (let i = selects.length; i > inputField_children.textContent; i--) {
+      windo.removeChild(document.getElementById("popUpselect_children"));
+    }
+  }
 }
