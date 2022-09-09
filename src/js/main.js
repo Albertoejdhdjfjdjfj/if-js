8fa518cab23f36c67e24fd29a9ee4642f4787797
Albert Bairamukov <90350959+Albertoejdhdjfjdjfj@users.noreply.github.com>
2022-09-09 22:17:31 +0300

    I corrected lesson-14

--- src/js/main.js
+++ src/js/main.js
@@ -1,58 +1,51 @@
-fetch("https://fe-student-api.herokuapp.com/api/hotels/popular")
-  .then((response) => response.json())
-  .then((data) => {
+window.onload = () => {
+  windowOnload();
+};
+
+function windowOnload() {
+  if (sessionStorage.getItem("data")) {
+    const data = JSON.parse(sessionStorage.getItem("data"));
     Slider(data);
-  });
+  } else {
+    fetch("https://fe-student-api.herokuapp.com/api/hotels/popular")
+      .then((response) => response.json())
+      .then((data) => {
+        sessionStorage.setItem("data", JSON.stringify(data));
+        Slider(data);
+      });
+  }
+}
 
 function Slider(data) {
-  console.log(data);
   const slider = document.getElementById("wrapper_for_category");
   const button = document.createElement("div");
   button.classList.add("button_arrow_right");
   button.innerHTML = "<svg><use xlink:href=\"#arrow_right\"></use></svg> ";
-  sliderImages(slider, data, button, 4);
+  sliderImages(slider, data, button, 0, 4);
+  changeImages(button, slider, data, 4);
   button.addEventListener("click", changeImages(slider, data, button, 4));
-
-  const cityInput = document.getElementById("city_input");
-  const searchButton = document.getElementById("searchButton");
-
-  searchButton.addEventListener("click", () => {
-    const sortdata = findmatches(cityInput.value, data);
-    slider.innerHTML = "";
-    sliderImages(slider, sortdata, button, sortdata.length);
-    changeImages(button, slider, sortdata, sortdata.length);
-  });
 }
 
-function sliderImages(blockbody, data, button, size) {
-  for (let start = 0; start < size; start++) {
+function sliderImages(blockbody, data, button, i, lenght) {
+  for (let start = i; start < lenght + i; start++) {
     const object = document.createElement("div");
     object.classList.add("object");
     object.innerHTML = `<a href=""><img src="${data[start].imageUrl}"></a><h3>${data[start].name}</h3> <p>${data[start].city}, ${data[start].country}</p>`;
     blockbody.appendChild(object);
+    if (start == data.lenght - lenght) {
+      break;
+    }
   }
 
   blockbody.appendChild(button);
 }
 
-function changeImages(blockbody, data, button, size) {
-  if (data.length > 4) {
-    let copydata = data;
-    copydata = data.concat(data);
-    return function () {
-      copydata.shift();
-      if (copydata.length == data.length) copydata = copydata.concat(data);
-
-      blockbody.innerHTML = "";
-      sliderImages(blockbody, copydata, button, size);
-      console.log(copydata);
-    };
-  }
-}
-
-function findmatches(string, data) {
-  return data.filter(
-    (item) =>
-      string.includes(item.name) || string.includes(item.country) || string.includes(item.city),
-  );
+function changeImages(blockbody, data, button, lenght) {
+  let start = lenght - 1;
+  return function () {
+    blockbody.innerHTML = "";
+    sliderImages(blockbody, data, button, start, lenght);
+    if (start == data.length - lenght) return (start = 0);
+    return start++;
+  };
 }
