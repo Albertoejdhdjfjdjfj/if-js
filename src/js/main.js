47f407f56bcaedc7e14ded2d24e12e4b04c4df70
Albert Bairamukov <90350959+Albertoejdhdjfjdjfj@users.noreply.github.com>
2022-09-18 19:53:53 +0300

    I corrected lesson-15

--- src/js/main.js
+++ src/js/main.js
@@ -1,51 +1,56 @@
-window.onload = () => {
-  windowOnload();
-};
+let input=document.getElementById('fileInput');
+let field=document.getElementById('fileField');
+let button=document.getElementById('fileButton');
+let img=document.getElementById('img');
+let form=document.getElementById('form');
 
-function windowOnload() {
-  const sessionStore = sessionStorage.getItem("data");
-  if (sessionStore) {
-    const data = JSON.parse(sessionStore);
-    Slider(data);
-  } else {
-    fetch("https://fe-student-api.herokuapp.com/api/hotels/popular")
-      .then((response) => response.json())
-      .then((data) => {
-        sessionStorage.setItem("data", JSON.stringify(data));
-        Slider(data);
-      });
-  }
+field.addEventListener('click',()=>{
+  input.click();
+  button.hidden=false;
+  onloadFile();
+  pushButton();
+})
+
+
+
+function onloadFile(){
+  input.addEventListener('change',()=>{
+    let file = input.files[0];
+
+  let reader = new FileReader();
+
+  reader.readAsDataURL(file);
+
+  reader.onload = function() {
+    console.log(reader.result);
+    img.setAttribute('src',reader.result)
+  };
+
+  reader.onerror = function() {
+    console.log(reader.error);
+  };
+  })
 }
 
-function Slider(data) {
-  const slider = document.getElementById("wrapper_for_category");
-  const button = document.createElement("div");
-  button.classList.add("button_arrow_right");
-  button.innerHTML = "<svg><use xlink:href=\"#arrow_right\"></use></svg> ";
-  sliderImages(slider, data, button, 4);
-  button.addEventListener("click", changeImages(slider, data, button, 4));
+function pushButton(){
+  button.addEventListener('click',()=>{
+
+    let response = fetch('https://fe-student-api.herokuapp.com/api/file', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      
+      body: new FormData(form),
+    });
+    if (response.ok) { 
+      alert('ok')
+    } else {
+      alert("Ошибка HTTP: " + response.status);
+    }
+  })
 }
 
-function sliderImages(blockBody, data, button, size) {
-  for (let start = 0; start < size; start++) {
-    const object = document.createElement("div");
-    object.classList.add("object");
-    object.innerHTML = `<a href="#"><img src="${data[start].imageUrl}" alt="${data[start].name}"></a><h3>${data[start].name}</h3> <p>${data[start].city}, ${data[start].country}</p>`;
-    blockBody.appendChild(object);
-  }
 
-  blockBody.appendChild(button);
-}
 
-function changeImages(blockBody, data, button, size) {
-  if (data.length > size) {
-    let copyData = data;
-    copyData = data.concat(data);
-    return function () {
-      copyData.shift();
-      if (copyData.length === data.length) copyData = copyData.concat(data);
-      blockBody.innerHTML = "";
-      sliderImages(blockBody, copyData, button, size);
-    };
-  }
-}
+
