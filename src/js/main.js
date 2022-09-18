bc14b5e0549def9b0aca39783f2aa4e447f4e2b0
Albert Bairamukov <90350959+Albertoejdhdjfjdjfj@users.noreply.github.com>
2022-09-18 19:56:00 +0300

    I corrected lesson-15

--- src/js/main.js
+++ src/js/main.js
@@ -1,56 +1,50 @@
-let input=document.getElementById('fileInput');
-let field=document.getElementById('fileField');
-let button=document.getElementById('fileButton');
-let img=document.getElementById('img');
-let form=document.getElementById('form');
+const input = document.getElementById("fileInput");
+const field = document.getElementById("fileField");
+const button = document.getElementById("fileButton");
+const img = document.getElementById("img");
+const form = document.getElementById("form");
 
-field.addEventListener('click',()=>{
+field.addEventListener("click", () => {
   input.click();
-  button.hidden=false;
+  button.hidden = false;
   onloadFile();
   pushButton();
-})
+});
 
+function onloadFile() {
+  input.addEventListener("change", () => {
+    const file = input.files[0];
 
+    const reader = new FileReader();
 
-function onloadFile(){
-  input.addEventListener('change',()=>{
-    let file = input.files[0];
+    reader.readAsDataURL(file);
 
-  let reader = new FileReader();
+    reader.onload = function () {
+      console.log(reader.result);
+      img.setAttribute("src", reader.result);
+    };
 
-  reader.readAsDataURL(file);
-
-  reader.onload = function() {
-    console.log(reader.result);
-    img.setAttribute('src',reader.result)
-  };
-
-  reader.onerror = function() {
-    console.log(reader.error);
-  };
-  })
+    reader.onerror = function () {
+      console.log(reader.error);
+    };
+  });
 }
 
-function pushButton(){
-  button.addEventListener('click',()=>{
-
-    let response = fetch('https://fe-student-api.herokuapp.com/api/file', {
-      method: 'POST',
+function pushButton() {
+  button.addEventListener("click", () => {
+    const response = fetch("https://fe-student-api.herokuapp.com/api/file", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      
+
       body: new FormData(form),
     });
-    if (response.ok) { 
-      alert('ok')
+    
+    if (response.ok) {
+      alert("ok");
     } else {
       alert("Ошибка HTTP: " + response.status);
     }
-  })
+  });
 }
-
-
-
-
