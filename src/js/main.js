997a42740da9b9045e6ac5faf54c41de9941b9a3
Albert Bairamukov <90350959+Albertoejdhdjfjdjfj@users.noreply.github.com>
2022-09-22 23:03:41 +0300

    I corrected lesson-16

--- src/js/main.js
+++ src/js/main.js
@@ -1,56 +1,102 @@
-const input = document.getElementById("fileInput");
-const field = document.getElementById("fileField");
-const button = document.getElementById("fileButton");
-const img = document.getElementById("img");
-const form = document.getElementById("form");
+const windo = document.getElementById("filter_form_body");
+windo.style.display = "none";
+const input = document.getElementById("num_people_input");
 
-field.addEventListener("click", () => {
-  input.click();
-  button.hidden = false;
-  onloadFile();
-  pushButton();
-});
+input.addEventListener("click", clickInput());
 
-function onloadFile() {
-  input.addEventListener("change", () => {
-    const file = input.files[0];
+const popUpLabel = document.getElementById("popUplabel_children");
+popUpLabel.style.display = "none";
 
-    const reader = new FileReader();
+const buttonPlus_adults = document.getElementById("plus_adults");
+const buttonMinus_adults = document.getElementById("minus_adults");
+const inputField_adults = document.getElementById("input_field_adults");
+changeNumber(buttonPlus_adults, buttonMinus_adults, inputField_adults, 30);
 
-    reader.readAsDataURL(file);
+const buttonPlus_children = document.getElementById("plus_children");
+const buttonMinus_children = document.getElementById("minus_children");
+const inputField_children = document.getElementById("input_field_children");
+changeNumber(buttonPlus_children, buttonMinus_children, inputField_children, 10, popUpLabel);
 
-    reader.onload = function () {
-      console.log(reader.result);
-      img.setAttribute("src", reader.result);
-    };
+const buttonPlus_rooms = document.getElementById("plus_rooms");
+const buttonMinus_rooms = document.getElementById("minus_rooms");
+const inputField_rooms = document.getElementById("input_field_rooms");
+changeNumber(buttonPlus_rooms, buttonMinus_rooms, inputField_rooms, 30);
 
-    reader.onerror = function () {
-      console.log(reader.error);
-    };
-  });
-}
+function changeNumber(buttonPlus, buttonMinus, inputField, max, popUpLabel = undefined) {
+  let i = inputField.textContent;
 
-function pushButton() {
-  button.addEventListener("click", () => {
-    const formData= new FormData(form);
-    formData.append("file",input.files[0]);
-    const response = fetch("https://fe-student-api.herokuapp.com/api/file", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-
-      body: formData
-    });
-    
-    console.log(formData)
-
-    if (response.ok) {
-      alert("ok");
+  buttonPlus.addEventListener("click", () => {
+    if (i < max) {
+      i++;
+      inputField.textContent = i;
     } else {
-      alert("Ошибка HTTP: " + response.status);
+      i = 0;
+      inputField.textContent = i;
     }
+
+    if (popUpLabel) {
+      popUpLabelAndSelect(inputField_children, popUpLabel);
+    }
+
+    inputParametrs();
+    selectNum();
+  });
+
+  buttonMinus.addEventListener("click", () => {
+    if (i > 0) {
+      i--;
+      inputField.textContent = i;
+    } else {
+      i = max;
+      inputField.textContent = i;
+    }
+    if (popUpLabel) {
+      popUpLabelAndSelect(inputField_children, popUpLabel);
+    }
+    inputParametrs();
+    selectNum();
   });
 }
 
+function popUpLabelAndSelect(inputField_children, popUpLabel) {
+  if (inputField_children.textContent != 0) {
+    popUpLabel.style.display = "inline";
+  } else if (inputField_children.textContent == 0) {
+    popUpLabel.style.display = "none";
+  }
+}
 
+function clickInput() {
+  let i = true;
+  return function () {
+    if (i) {
+      windo.style.display = "flex";
+      i = !i;
+    } else {
+      windo.style.display = "none";
+      i = !i;
+    }
+  };
+}
+
+function inputParametrs() {
+  input.textContent = `${inputField_adults.textContent} Adults — ${inputField_children.textContent} Children — ${inputField_rooms.textContent} Room`;
+}
+
+function selectNum() {
+  const selects = document.getElementsByClassName("popUpselect_children");
+  if (selects.length < inputField_children.textContent) {
+    for (let i = selects.length; i < inputField_children.textContent; i++) {
+      const popUpSelect = document.createElement("select");
+      popUpSelect.classList = "popUpselect_children";
+      popUpSelect.id = "popUpselect_children";
+      popUpSelect.innerHTML =
+        "<option>0 years old</option><option>1 years old</option><option>2 years old</option><option>3 years old</option><option>4 years old</option><option>5 years old</option><option>6 years old</option><option>7 years old</option><option>8 years old</option>";
+      windo.appendChild(popUpSelect);
+    }
+  } else if (selects.length > inputField_children.textContent) {
+    for (let i = selects.length; i > inputField_children.textContent; i--) {
+      windo.removeChild(document.getElementById("popUpselect_children"));
+    }
+  }
+}
